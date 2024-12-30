de220982c1286b163c0acc6c44612603dc8d5d98
khaledmoussaaa <khaledmoussa909@gmail.com>
2024-12-30 15:47:40 +0200

    update

---
+++ app/Http/Controllers/Reviews/ReviewController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers\Reviews;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Reviews\ReviewRequest;
+use App\Models\Review;
+use Illuminate\Http\Request;
+
+class ReviewController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        $reviews = Review::nurseryScope($request)->get();
+        return contentResponse($reviews);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(ReviewRequest $request)
+    {
+        $review = Review::create($request->validated());
+        return messageResponse();
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Review $review)
+    {
+        return contentResponse($review);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(ReviewRequest $request, Review $review)
+    {
+        $review->update($request->validated());
+        return messageResponse();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Review $review)
+    {
+        $review->forceDelete();
+        return messageResponse();
+    }
+}
