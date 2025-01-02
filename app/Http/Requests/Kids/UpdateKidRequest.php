1b815e86d3337f656dae20576a08d9445ffed7cc
khaledmoussaaa <khaledmoussa909@gmail.com>
2025-01-02 16:58:44 +0200

    Fix bugs

--- app/Http/Requests/Kids/UpdateKidRequest.php
+++ app/Http/Requests/Kids/UpdateKidRequest.php
@@ -26,7 +26,7 @@ class UpdateKidRequest extends FormRequest
             'last_name' => 'required|string',
             'gender' => 'required|in:boy,girl',
             'birth_date' => 'required|date',
-            'has_medical_case' => 'nullable|string|in:yes,no',
+            'has_medical_case' => 'nullable|integer|in:0,1',
             'description_medical_case' => 'required_with:has_medical_case|string',
             'class_room_id' => 'required|integer|exists:class_rooms,id',
             'branch_id' => 'required|integer|exists:branches,id',
